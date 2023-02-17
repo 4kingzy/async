b0a7173a30516de70ca3f718256dcf3f66af95ea
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2023-02-17 20:41:19 +0100

    Add files via upload

--- callback.js
+++ callback.js
@@ -99,3 +99,23 @@ buildSomething("Foundations", 1000)
     .catch((err) => {
         console.log(err);
     });
+
+/*4 PROMISE example*/
+const promise1 = new Promise ((resolve, reject) => {
+    setTimeout(function() {
+        resolve("Promise 1 finished.")
+    }, 2000);
+})
+
+const promise2 = new Promise ((resolve, reject) => {
+    setTimeout(function() {
+        resolve("Promise 2 finished.")
+    }, 5000);
+})
+
+promise1.then(result => console.log(result));
+
+Promise.all([promise1, promise2])
+.then(res => console.log(res))
+.catch(err => console.log(err))
+.finally(() => console.log("Always executed"))
